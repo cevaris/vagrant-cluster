5240763f024da100118db767368dec559572065f
Cevaris <cevaris@gmail.com>
2013-11-24 14:39:03 -0700

    Added load config and config file

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,12 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+require './scripts/load_config'
+
+BASE_DIR = File.expand_path(File.dirname(__FILE__))
+CONFIG   = load_config(YAML.load(File.read(File.join(base_dir, "config.yml"))))
+
+
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
@@ -19,7 +25,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
-  # config.vm.network :forwarded_port, guest: 80, host: 8080
+  # config.vm.network :forwarded_port, guest: 80800, host: 8080
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
---
+++ scripts/load_config.rb
@@ -0,0 +1,31 @@
+def gen_node_infos(cluster_yml)
+  master_n = cluster_yml['master_n']
+  master_mem = cluster_yml['master_mem']
+  slave_n = cluster_yml['slave_n']
+  slave_mem = cluster_yml['slave_mem']
+  force_zk = cluster_yml['force_zk']
+  zk_mem = cluster_yml['zk_mem']
+  master_ipbase = cluster_yml['master_ipbase']
+  slave_ipbase = cluster_yml['slave_ipbase']
+  zk_ipbase = cluster_yml['zk_ipbase']
+
+  master_infos = (1..master_n).map do |i|
+                   { :hostname => "master#{i}",
+                     :ip => master_ipbase + "#{10+i}",
+                     :mem => master_mem }
+                 end
+  slave_infos = (1..slave_n).map do |i|
+                   { :hostname => "slave#{i}",
+                     :ip => slave_ipbase + "#{10+i}",
+                     :mem => slave_mem }
+                 end
+
+  zk_n = master_n > 1 ? 3 : (force_zk ? 1 : 0)
+  zk_infos = (1..zk_n).map do |i|
+               { :hostname => "zk#{i}",
+                 :ip => zk_ipbase + "#{10+i}",
+                 :mem => zk_mem }
+             end
+
+  return { :master => master_infos, :slave=>slave_infos, :zk=>zk_infos }
+end
